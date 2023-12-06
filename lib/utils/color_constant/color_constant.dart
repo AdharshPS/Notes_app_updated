bb7564efd29aae714302efba91afc15869e242a7
AdharshPS <adharshps000@gmail.com>
2023-12-06 15:29:12 +0530

    color

--- lib/utils/color_constant/color_constant.dart
+++ lib/utils/color_constant/color_constant.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ColorConstant {
-  static Color backgroundColor = Color(0xFFFAF0E6);
+  static Color backgroundColor = Color(0xFFFFFFFF);
   static Color bottomSheet = Color(0xFFB9B4C7);
   static Color splash = Color(0xFF5C5470);
   static Color titleColor = Color(0xFF352F44);
